a1ba6821c7ef5d0a40701876e22beba545be2465
Toh Hong Xiang <tohhongxiang@gmail.com>
2026-03-24 23:50:20 +0800

    feat: Persist last selected language

--- src/components/problem/use-problem-code-editor.tsx
+++ src/components/problem/use-problem-code-editor.tsx
@@ -1,17 +1,22 @@
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 
 import useLocalStorageState from "@/hooks/use-local-storage-state";
 import type { Language } from "@/lib/language/types";
 import type { Problem } from "@/lib/problem/types";
 
+const LAST_USED_LANGUAGE_STORAGE_KEY = "editor:last-language";
 export default function useProblemCodeEditor(problem?: Problem) {
-	const [selectedLanguageID, setSelectedLanguageID] =
-		useState<Language["id"]>();
-
 	const availableLanguages = useMemo(
 		() => problem?.starterCode.map((s) => s.language) ?? [],
 		[problem],
 	);
+
+	// Use the last language that the user selected, default to the first language in the available languages array
+	const [selectedLanguageID, setSelectedLanguageID] = useLocalStorageState(
+		LAST_USED_LANGUAGE_STORAGE_KEY,
+		availableLanguages[0]?.id,
+	);
+
 	const selectedLanguage =
 		availableLanguages.find((l) => l.id === selectedLanguageID) ??
 		availableLanguages[0];
@@ -19,7 +24,7 @@ export default function useProblemCodeEditor(problem?: Problem) {
 	const starterCode = useMemo(
 		() =>
 			problem?.starterCode.find(
-				(s) => s.language.id === selectedLanguage?.id,
+				(s) => s.language.id === selectedLanguage?.id, // use selectedLanguage.id here instead of selectedLanguageID to allow fallback behavior
 			)?.code ?? "",
 		[problem, selectedLanguage],
 	);
